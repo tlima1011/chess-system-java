de0f79e4d67234a8f83dc96290d165ff3c701304
Thiago <thiagolima1011@gmail.com>
2020-12-28 18:42:18 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,46 +8,27 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
-	private Board board;
+private Board board;
 	
 	public ChessMatch() {
-		board = new Board(8,8); 
+		board = new Board(8, 8);
 		initialSetup();
 	}
 	
-	public ChessPiece[][] getPieces(){
-		ChessPiece [][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for(int i = 0; i < board.getRows();i++) {
-			for(int j = 0; j < board.getColumns();j++) {
-				mat[i][j] = (ChessPiece) board.piece(i,j);
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i=0; i<board.getRows(); i++) {
+			for (int j=0; j<board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
 			}
 		}
 		return mat;
 	}
 	
-	public ChessPiece performChessMovie(ChessPosition sourcePosition, ChessPosition targetPosition) {
-		Position source = sourcePosition.toPosition(); 
-		Position target = targetPosition.toPosition(); 
-		validateSourcePosition(source); 
-		Piece capturedPiece = makeMove(source, target);
-		return (ChessPiece) capturedPiece;
-	}
-	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)) {
-			throw new ChessException("There is no piece on source position"); 
-		}
-	}
-	
-	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source); 
-		Piece capturedPiece = board.removePiece(target);
-		board.placePiece(p, target);
-		return (ChessPiece) capturedPiece; 
-	}
-	
-	private void placeNewPiece(char column, int row, ChessPiece piece) { 
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
+	
 	private void initialSetup() {
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
@@ -62,5 +43,10 @@ public class ChessMatch {
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
+	}
+
+	public ChessPiece performChessMovie(ChessPosition source, ChessPosition target) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -5,14 +5,14 @@ import boardgame.Position;
 public class ChessPosition {
 	
 	private char column;
-	private int row; 
-		
+	private int row;
+	
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
-		this.row = row; 
 		this.column = column;
+		this.row = row;
 	}
 
 	public char getColumn() {
@@ -22,17 +22,17 @@ public class ChessPosition {
 	public int getRow() {
 		return row;
 	}
-	
+
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
+	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition( (char )('a' - position.getColumn()), 8 - position.getRow());
-		
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
 	}
-		
+	
 	@Override
 	public String toString() {
-		return "" + column + "row";
+		return "" + column + row;
 	}
 }
