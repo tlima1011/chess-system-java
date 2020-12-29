c7a0cbf6680d5795a7a6d963b2aad49d37ddc0db
Thiago <thiagolima1011@gmail.com>
2020-12-29 00:00:13 -0300

    Implementing again possible moves of Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -52,25 +52,21 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println(" a b c d e f g h");
+		System.out.println("  a b c d e f g h");
 	}
 
 	private static void printPiece(ChessPiece piece) {
-		if (piece == null) {
-			System.out.print("-");
-		} else {
-			System.out.print(piece);
-		}
-		System.out.print(" ");
-		if (piece == null) {
-			System.out.print("-");
-		} else {
-			if (piece.getColor() == Color.WHITE) {
-				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-			} else {
-				System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
-			}
-		}
-		System.out.print(" ");
+    	if (piece == null) {
+            System.out.print("-");
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
 }
