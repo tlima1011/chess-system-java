95d23bc22d0d9b349b8259d545773f98c2076842
Thiago <thiagolima1011@gmail.com>
2021-01-01 20:11:20 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,11 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/Q/R): ");
+					String type = sc.nextLine(); 
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
